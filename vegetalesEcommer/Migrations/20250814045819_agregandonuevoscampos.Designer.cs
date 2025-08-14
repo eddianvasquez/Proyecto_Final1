0af896e1b89020d47c150e2853568c247de168a2
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-08-14 09:20:30 -0400

    Migraciones

--- vegetalesEcommer/Migrations/20250814045819_agregandonuevoscampos.Designer.cs
+++ vegetalesEcommer/Migrations/20250814045819_agregandonuevoscampos.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proyecto_Final1.Data;
 
@@ -11,9 +12,11 @@ using Proyecto_Final1.Data;
 namespace Proyecto_Final1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250814045819_agregandonuevoscampos")]
+    partial class agregandonuevoscampos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -370,28 +373,15 @@ namespace Proyecto_Final1.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ProductoId"));
 
-                    b.Property<string>("Categoria")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<string>("Descripcion")
                         .IsRequired()
-                        .HasMaxLength(500)
-                        .HasColumnType("nvarchar(500)");
-
-                    b.Property<bool>("EsOrganicoCertificado")
-                        .HasColumnType("bit");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("ImagenUrl")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Nombre")
-                        .IsRequired()
-                        .HasMaxLength(100)
-                        .HasColumnType("nvarchar(100)");
-
-                    b.Property<string>("Origen")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
@@ -401,13 +391,6 @@ namespace Proyecto_Final1.Migrations
 
                     b.Property<int>("Stock")
                         .HasColumnType("int");
-
-                    b.Property<string>("Temporada")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("UnidadDeMedida")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("ProductoId");
 
---
+++ vegetalesEcommer/Migrations/20250814050349_agregandocomponentes.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_Final1.Migrations
+{
+    /// <inheritdoc />
+    public partial class agregandocomponentes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Productos",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Productos",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Categoria",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EsOrganicoCertificado",
+                table: "Productos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Origen",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Temporada",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UnidadDeMedida",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "EsOrganicoCertificado",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "Origen",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "Temporada",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "UnidadDeMedida",
+                table: "Productos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
